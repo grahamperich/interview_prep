897bb2ce093911f92d194cc661a34e55089a8248
GrahamPerich <graham.perich@gmail.com>
2017-02-20 17:01:41 -0800

    complete rectangleArea

---
+++ rectangleArea.js
@@ -0,0 +1,23 @@
+// Write a function to find the rectangular intersection of two given love rectangles.
+
+// As with the example above, love rectangles are always "straight" and never "diagonal." More rigorously: each side is parallel with either the x-axis or the y-axis.
+
+// They are defined as objects ↴ like this :
+
+//   var myRectangle = {
+
+//     // coordinates of bottom-left corner
+//     leftX: 1,
+//     bottomY: 5,
+
+//     // width and height
+//     width: 10,
+//     height: 4,
+
+// };
+
+// Your output rectangle should use this format as well.
+
+function calcOverlapArea(rectOne, rectTwo) {
+  
+}
