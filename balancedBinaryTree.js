611ac5aa2b0bc5c1d3826a2d9629df695f73ef0d
GrahamPerich <graham.perich@gmail.com>
2017-02-22 19:41:22 -0800

    implement iterative solution for balancedbinarytree

--- balancedBinaryTree.js
+++ balancedBinaryTree.js
@@ -18,9 +18,48 @@ BinaryTreeNode.prototype.insertRight = function(value) {
     return this.right;
 };
 
+// iterative depth-first linear time solution
+function superBalance(firstNode) {
+  let nodes = [];
+  nodes.push([firstNode, 0]);
+  let leafDepths = [];
+
+  while (nodes.length) {
+    let tuple = nodes.pop();
+    let node = tuple[0]
+    let depth = tuple[1];
+
+    if (node.left) {
+      nodes.push([node.left, depth+1]);
+    }
+
+    if (node.right) {
+      nodes.push([node.right, depth+1]);
+    }
+
+    // we found a leaf
+    if (!node.right && !node.left) {
+      // if we haven't encountered this depth yet then add it
+      if (leafDepths.indexOf(depth) < 0) {
+        leafDepths.push(depth);
+        // check for two cases in which the tree is not balanced:
+        // if there are more than two different depths 
+        // if there are exactly two depths but their difference is > 1
+        if ((leafDepths.length > 2) || Math.abs(leafDepths[0] - leafDepths[1]) > 1) {
+          return false;
+        }
+      }
+    }
+
+  } 
+
+  return true;
+}
+
+// recursive depth-first linear time solution 
 function isSuperBalanced(tree) {
-  var isBalanced = true;
-  var leafDepths = [];
+  let isBalanced = true;
+  let leafDepths = [];
 
   function inner(node, depth) {
     if (node.left) {
